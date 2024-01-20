4ecdee99181108af55e47854e04ec517c1339b4a
kmHu <626694157@qq.com>
2024-01-20 01:27:00 -0500

    read the docs

---
+++ _build/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hkm/CirSAT/main.cpp" "CMakeFiles/run.dir/main.cpp.o" "gcc" "CMakeFiles/run.dir/main.cpp.o.d"
+  "/home/hkm/CirSAT/src/aig.cpp" "CMakeFiles/run.dir/src/aig.cpp.o" "gcc" "CMakeFiles/run.dir/src/aig.cpp.o.d"
+  "/home/hkm/CirSAT/src/parser.cpp" "CMakeFiles/run.dir/src/parser.cpp.o" "gcc" "CMakeFiles/run.dir/src/parser.cpp.o.d"
+  "/home/hkm/CirSAT/src/solver.cpp" "CMakeFiles/run.dir/src/solver.cpp.o" "gcc" "CMakeFiles/run.dir/src/solver.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _build/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/main.cpp.o"
+  "CMakeFiles/run.dir/main.cpp.o.d"
+  "CMakeFiles/run.dir/src/aig.cpp.o"
+  "CMakeFiles/run.dir/src/aig.cpp.o.d"
+  "CMakeFiles/run.dir/src/parser.cpp.o"
+  "CMakeFiles/run.dir/src/parser.cpp.o.d"
+  "CMakeFiles/run.dir/src/solver.cpp.o"
+  "CMakeFiles/run.dir/src/solver.cpp.o.d"
+  "run"
+  "run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
